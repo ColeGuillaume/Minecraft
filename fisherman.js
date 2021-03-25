1c48bcf9463fcc7b03796a62ac35e29a877ead23
micahodell22 <55461490+micahodell22@users.noreply.github.com>
2021-03-24 21:48:58 -0400

    Create fisherman.js

---
+++ fisherman.js
@@ -0,0 +1,80 @@
+const mineflayer = require("mineflayer");
+const bot = mineflayer.createBot({ host: 'localhost', port: '51293', username: "Player" });
+
+let mcData
+bot.on('inject_allowed', () => {
+  mcData = require('minecraft-data')(bot.version)
+})
+
+// To fish we have to give bot the fishing rod and teleport bot to the water
+// /give fisherman fishing_rod 1
+// /teleport fisherman ~ ~ ~
+
+// To eat we have to apply hunger first
+// /effect fisherman minecraft:hunger 1 255
+
+bot.on('message', (cm) => {
+  if (cm.toString().includes('start')) {
+    startFishing()
+  }
+
+  if (cm.toString().includes('stop')) {
+    stopFishing()
+  }
+
+  if (cm.toString().includes('eat')) {
+    eat()
+  }
+})
+
+let nowFishing = false
+
+function onCollect (player, entity) {
+  if (entity.kind === 'Drops' && player === bot.entity) {
+    bot.removeListener('playerCollect', onCollect)
+    startFishing()
+  }
+}
+
+async function startFishing () {
+  bot.chat('Fishing')
+  try {
+    await bot.equip(mcData.itemsByName.fishing_rod.id, 'hand')
+  } catch (err) {
+    return bot.chat(err.message)
+  }
+
+  nowFishing = true
+  bot.on('playerCollect', onCollect)
+
+  try {
+    await bot.fish()
+  } catch (err) {
+    bot.chat(err.message)
+  }
+  nowFishing = false
+}
+
+function stopFishing () {
+  bot.removeListener('playerCollect', onCollect)
+
+  if (nowFishing) {
+    bot.activateItem()
+  }
+}
+
+async function eat () {
+  stopFishing()
+
+  try {
+    await bot.equip(mcData.itemsByName.fish.id, 'hand')
+  } catch (err) {
+    return bot.chat(err.message)
+  }
+
+  try {
+    await bot.consume()
+  } catch (err) {
+    return bot.chat(err.message)
+  }
+}
