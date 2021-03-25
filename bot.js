72824bd2927d10f5e6c7081f4cce2012f36b6de9
cguillau <Ctrain137>
2021-03-25 12:53:16 -0400

    first

--- bot.js
+++ bot.js
@@ -1,15 +1,21 @@
 // Create your bot
 const mineflayer = require("mineflayer");
-const bot = mineflayer.createBot({ host: 'localhost', port: '51293', username: "Player" });
+const bot = mineflayer.createBot({ host: 'localhost', port: '52858', username: "Player" });
 
 // Load your dependency plugins.
 bot.loadPlugin(require('mineflayer-pathfinder').pathfinder);
 
 // Import required behaviors.
 const {
+    globalSettings,
     StateTransition,
     BotStateMachine,
+    StateMachineWebserver,
     EntityFilters,
+    BehaviorIdle,
+    BehaviorFindBlock,
+    BehaviorMoveTo,
+    BehaviorPrintServerStats,
     BehaviorFollowEntity,
     BehaviorLookAtEntity,
     BehaviorGetClosestEntity,
@@ -20,14 +26,47 @@ bot.once("spawn", () =>
 {
     // This targets object is used to pass data between different states. It can be left empty.
     const targets = {};
+    const water = {};
 
     // Create our states
+    const printServerStates = new BehaviorPrintServerStats(bot)
+    const findWater = new BehaviorFindBlock(bot, targets)
+    const goToWater = new BehaviorMoveTo(bot, targets)
+    const idle = new BehaviorIdle()
+
     const getClosestPlayer = new BehaviorGetClosestEntity(bot, targets, EntityFilters().PlayersOnly);
     const followPlayer = new BehaviorFollowEntity(bot, targets);
-    const lookAtPlayer = new BehaviorLookAtEntity(bot, targets);
+    const LookAtPlayer = new BehaviorLookAtEntity(bot, targets);
+    //const placeBlock = new BehaviorPlaceBlock(bot, targets);
 
     // Create our transitions
     const transitions = [
+
+
+        new StateTransition({ // 0
+            parent: printServerStates,
+            child: findWater,
+            shouldTransition: () => true
+        }),
+
+        new StateTransition({ // 0
+            parent: findWater,
+            child: goToWater,
+            shouldTransition: () => true
+        }),
+
+        new StateTransition({ // 0
+            parent: goToWater,
+            child: idle,
+            shouldTransition: () => goToWater.isFinished()
+        }),
+
+
+        // new StateTransition({ // 0
+        //     parent: printServerStates,
+        //     child: getClosestPlayer,
+        //     shouldTransition: () => true
+        //   }),
 
         // We want to start following the player immediately after finding them.
         // Since getClosestPlayer finishes instantly, shouldTransition() should always return true.
@@ -41,23 +80,25 @@ bot.once("spawn", () =>
         // state to the lookAtPlayer state.
         new StateTransition({
             parent: followPlayer,
-            child: lookAtPlayer,
+            child: LookAtPlayer,
             shouldTransition: () => followPlayer.distanceToTarget() < 2,
         }),
 
         // If the distance to the player is more than two blocks, switch from the lookAtPlayer
         // state to the followPlayer state.
         new StateTransition({
-            parent: lookAtPlayer,
+            parent: LookAtPlayer,
             child: followPlayer,
-            shouldTransition: () => lookAtPlayer.distanceToTarget() >= 2,
+            shouldTransition: () => LookAtPlayer.distanceToTarget() >= 2,
         }),
     ];
 
     // Now we just wrap our transition list in a nested state machine layer. We want the bot
     // to start on the getClosestPlayer state, so we'll specify that here.
-    const rootLayer = new NestedStateMachine(transitions, getClosestPlayer);
+    const rootLayer = new NestedStateMachine(transitions, printServerStates);
     
     // We can start our state machine simply by creating a new instance.
-    new BotStateMachine(bot, rootLayer);
+    const stateMachine = new BotStateMachine(bot, rootLayer)
+    const webserver = new StateMachineWebserver(bot, stateMachine)
+    webserver.startServer()
 });
