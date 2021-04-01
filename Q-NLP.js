8a1c0e08d3f90b6a9fc11417fed231e368927f1f
partainq <partainq@gmail.com>
2021-03-31 20:35:47 -0400

    working on that nlp. up and running bot.

--- Q-NLP.js
+++ Q-NLP.js
@@ -1,27 +1,60 @@
 // Minecraft Psychologist
 // Cole, Micah, Quinn
+const mineflayer = require("mineflayer");
+const bot = mineflayer.createBot({ host: 'localhost', port: '50665', username: "FinalBot" });
 
-var natural = require('natural');
-var Analyzer = require('natural').SentimentAnalyzer;
-var stemmer = require('natural').PorterStemmer;
+const natural = require('natural');
+const Analyzer = require('natural').SentimentAnalyzer;
+const stemmer = require('natural').PorterStemmer;
 
-var YES_NO_FLAG;
-var STEMMER_SCORE;
+let NAME_FLAG = true;
+let YES_NO_FLAG = false;
+let STEMMER_SCORE = 0;
 
+bot.on('login', () => {
+    bot.chat('Hello. I am your physcologist. You may call me Dr.')
+    bot.chat('What is your name?');
+});
+
+bot.on('playerLeft', (player) => {
+    if (player.username === bot.username) return
+    bot.chat(`Bye ${player.username}`)
+});
+
+bot.on('chat', (username, message) => {
+    if (username != bot.username) {
+        if (NAME_FLAG) {
+            let tokenizer = new natural.WordTokenizer();
+            let x = tokenizer.tokenize(message);
+            if (x.length == 1) { 
+                bot.chat(`Nice to meet you ${ x }.`);
+                NAME_FLAG = false;
+            }
+            else {
+                bot.chat('Give me your first name, and only your first name please.');
+            }
+        }
+        // Quiz time!
+        if (YES_NO_FLAG) {
+            console.log('quiz');
+            conductQuiz();
+        }
+    }
+});
+
+function conductQuiz() {
+    console.log('quiz');
+    // while true of 5 questions
+    // pops a question out of an array
+    // records answer
+    // uses stemmer to add 1 or 2 to STEMMER SCORE
+}
 // TODO
 // Create 2 lists, each length 5. One list for random responses to postive stemmer, another for negative.
 // Create 1 list of yes/no questions that are physology related, length 20. we pick random 5.
 // Create ending remarks of using global VAR Stemmer Score
 
 // Put it in minecraft!
-
-// Get Name
-var tokenizer = new natural.WordTokenizer();
-var x = tokenizer.tokenize("Bob");
-if (x.length == 1) { console.log(`Nice to meet you ${ x }.`)}
-else {
-    console.log('Give me your first name, and only your first name please.')
-}
 
 // If more then one Sentence
 tokenizer = new natural.SentenceTokenizerNew();
